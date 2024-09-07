866f10b53ed92517e25ab203966a2bd38c6b449c
Jadeiscodingg <drydenjade77@gmail.com>
2024-09-07 18:18:01 +0200

    Sphinx change

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'shop.settings'
+sys.path.insert(0, os.path.abspath('../../'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bakery.settings'
 django.setup()
 # Configuration file for the Sphinx documentation builder.
 #
