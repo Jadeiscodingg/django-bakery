6c5d3f19a46f23227e7ec0fb6c90cb9ad79d8805
Jadeiscodingg <drydenjade77@gmail.com>
2024-09-07 17:28:30 +0200

    Added/update with docstrings

--- shop/apps.py
+++ shop/apps.py
@@ -2,6 +2,16 @@ from django.apps import AppConfig
 
 
 class ShopConfig(AppConfig):
+    """
+    Configuration class for the 'shop' application.
+
+    This class provides configuration for the 'shop' Django application,
+    including settings for the default auto field type used for models.
+
+    Attributes:
+        default_auto_field (str): The type of auto field to use for models in the application.
+        name (str): The name of the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
 
